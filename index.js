a3a6943d2bb9a088de912163a5170cd00fc2d9da
hifzazunair7 <hifzazunair7@gmail.com>
2023-05-17 13:37:33 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var goods = prompt("enter your goods");
+if (goods ==="watch"|| goods =="Watch"){
+    console.log("watch is available"); 
+}
+else if (goods =="clock"){
+    console.log("clock is available");
+}
+else if (goods=="mobile"){
+    console.log("mobile is avalable");
+}
+else if (goods=="pods"){
+    console.log("out of stock");
+}
+else{
+    console.log("not available")
+}
